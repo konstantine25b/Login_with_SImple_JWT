4b4b101a4dac1bb3e297a4e2031b20d18233de6b
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-07 12:50:22 +0400

    added cors headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    "corsheaders",
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist', # es ro refresh tokeni dablacklistdes
 ]
@@ -87,7 +88,8 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -166,3 +168,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS =True
